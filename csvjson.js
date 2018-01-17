6e3d6ab60719216f29e67a665116b41ef5110dd8
w-22 <33657553+w-22@users.noreply.github.com>
2018-01-17 17:58:16 +0100

    Create csvjson.js

---
+++ csvjson.js
@@ -0,0 +1,38 @@
+// Dependencies
+const fs = require('fs')
+const path = require('path')
+const csv = require('csvtojson')
+
+const csvFile = path.join(__dirname, 'customer-data.csv')
+const jsonFile = path.join(__dirname, 'customer-data.json')
+
+// temporary array for output data
+const output = []
+
+
+// add each row to the array
+const loadRow = (row) => {
+  output.push(row)
+}
+
+// write to a file
+const writeJson = (err) => {
+  if (err) {
+    throw err
+  }
+
+  const content = JSON.stringify(output, null, 2)// convert from the array to JSON string
+
+  fs.writeFile(jsonFile, content, 'utf8', (err) => {
+    if (err) {
+      throw err
+    }
+    console.log(`Json file saved to ${jsonFile}`)
+  })
+}
+
+// execute
+csv()
+  .fromFile(csvFile)// read from csv file
+  .on('json', loadRow)// update array on every row
+  .on('done', writeJson)// create and save json file
